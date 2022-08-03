f7c6c864499f69f2c5967bc258617dca600343f0
santosh33 <santosh33@gmail.com>
2022-08-03 19:02:12 +0530

    Wire up provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createRoot, createStore } from 'redux';
+import App from './components/App';
+import reducers from './reducers';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>
 );
