1b72c8d4f0b68f91f62443ad3751085bde3bf5df
santosh <santosh33@gmail.com>
2022-08-19 13:20:24 +0530

    conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import SongList from "./SongList";
+import songDetail from "./songDetail";
+import SongDetail from "./songDetail";
 
 const App = () => {
   return (
@@ -7,6 +9,9 @@ const App = () => {
       <div className="ui row">
         <div className="column eight wide">
           <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
         </div>
       </div>
     </div>
